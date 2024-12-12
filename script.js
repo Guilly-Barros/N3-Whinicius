302e73dbe07de93f9d560a60b3d9d560121e0c76
Guilly_Barros <guilherme.passos.01712@alunos.unialfa.com.br>
2024-12-12 16:19:02 -0300

    Atualização 01

---
+++ script.js
@@ -0,0 +1,18 @@
+// Seleciona o botão de alternância e a área do menu
+const toggleButton = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('#navbarNav');
+
+// Adiciona um evento de clique ao botão
+toggleButton.addEventListener('click', () => {
+    const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+
+    // Alterna o atributo aria-expanded e a classe "show"
+    toggleButton.setAttribute('aria-expanded', !isExpanded);
+    navbarCollapse.classList.toggle('show');
+});
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
